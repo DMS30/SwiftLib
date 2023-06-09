f0bcb2dd6f79bd884173a953f2fafcb7514d4ee7
inai-shanmukh <119917403+inai-shanmukh@users.noreply.github.com>
2023-06-09 18:15:53 +0530

    Added Test Cases

---
+++ SwiftLib/SwiftLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Shanmukh DM on 09/06/23.
+//
+
+public final class SwiftLib {
+
+    let name = "SwiftLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
