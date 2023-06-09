da7d72d2ca43d8e0159f75209fcb8cf3e8bdc97c
inai-shanmukh <119917403+inai-shanmukh@users.noreply.github.com>
2023-06-09 18:11:17 +0530

    Initial commit

---
+++ SwiftLib/SwiftLib.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLib.h
+//  SwiftLib
+//
+//  Created by Shanmukh DM on 09/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLib.
+FOUNDATION_EXPORT double SwiftLibVersionNumber;
+
+//! Project version string for SwiftLib.
+FOUNDATION_EXPORT const unsigned char SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib/PublicHeader.h>
+
+
